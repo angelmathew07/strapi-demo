df191fd6b430dc7657c3c19b7680b9d5c603df79
angel_anna.matthew <angel_anna.mathew@bell.ca>
2020-12-18 15:55:02 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bc96b3f6e424319c4ae6d690e2635535'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ab6036c2-8607-4abd-b3ec-7f1921afb249'
+};
